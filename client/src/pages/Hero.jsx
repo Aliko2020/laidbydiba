5d643bba03c79182c7b6d0191791bbf6785cbdfe
Aliko Amos <alikoamos7@gmail.com>
2025-01-09 15:15:51 -0500

    ui changess updates

--- client/src/pages/Hero.jsx
+++ client/src/pages/Hero.jsx
@@ -10,7 +10,7 @@ const flickityOptions = {
 const Hero = () => {
 
   return (
-    <div className="flex flex-col text-white w-full items-center justify-center min-h-[100vh] text-center p-4 hero">
+    <div className="flex flex-col text-white w-full items-center justify-center min-h-[100vh] text-center p-4 bg-center bg-cover bg-[linear-gradient(90deg,rgba(99,115,115,1)_0%,rgba(99,115,115,0.38699229691876746)_99%),url('./assets/back.png')]">
       <h1 className="text-5xl font-bold mb-4 font-test mt-4 animation">Welcome To Laidbydiba</h1>
       <p className="text-lg mb-2">
         Your go-to destination for stylish and professional hair services. Book your appointment today!
